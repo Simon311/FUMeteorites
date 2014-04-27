19acebc63e1a21a5e015e8b5597cc413f78e462e
Simon <mkontent@gmail.com>
2014-04-27 12:58:40 +0400

    Got rid of the fucking space

--- FUMeteorites/Properties/AssemblyInfo.cs
+++ FUMeteorites/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("FU Meteorites")]
+[assembly: AssemblyTitle("FUMeteorites")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Community")]
